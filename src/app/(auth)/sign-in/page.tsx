182f020986a025f62f502d585fb82def90645ae5
omar <o.wakeel@intouch.com>
2024-01-08 18:23:18 +0200

    feat: create auth pages

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import TextFieldInput from "@/components/data/TextFieldInput";
+import AuthContainer from "@/components/presentational/AuthContainer";
+import Link from "next/link";
+import { FormProvider, useForm } from "react-hook-form";
+
+export default function SignIn() {
+    const methods = useForm()
+
+    return (
+        <AuthContainer title="Sign in to your account">
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <FormProvider {...methods} >
+                    <form className="space-y-6" action="#" method="POST">
+                        <TextFieldInput name="email" label="Email" />
+                        <TextFieldInput name="password" label="Password" />
+                        <div>
+                            <button type="submit" className="flex w-full justify-center rounded-md bg-teal-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-teal-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                        </div>
+                    </form>
+                </FormProvider>
+
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    <span>Don't have an account, </span>
+                    <Link href="sign-up" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign up</Link>
+                </p>
+            </div>
+        </AuthContainer>
+    )
+}
---
+++ src/components/data/TextFieldInput.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import React from 'react'
+import { Controller, useFormContext } from "react-hook-form";
+import TextField from "@mui/material/TextField";
+
+interface Props {
+    name: string;
+    label: string
+}
+
+const TextFieldInput: React.FC<Props> = (props) => {
+    const { name, label } = props;
+
+    const methods = useFormContext();
+    const { control } = methods;
+
+    return (
+        <Controller
+            name={name}
+            control={control}
+            render={({
+                field: { onChange, value },
+                fieldState: { error },
+            }) => (
+                <TextField
+                    helperText={error ? error.message : null}
+                    size="small"
+                    error={!!error}
+                    onChange={onChange}
+                    value={value}
+                    fullWidth
+                    label={label}
+                    variant="outlined"
+                />
+            )}
+        />
+    )
+}
+
+export default TextFieldInput;
